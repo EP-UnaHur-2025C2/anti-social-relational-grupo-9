989687c477a337450ccca5376c7cdc605bde4203
AsamFernando <ferasam88@gmail.com>
2025-10-05 18:09:38 -0300

    primer commit, creados modelos y servidor con express, conexion a base de datos

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express');
+const db = require('./db/models');
+const PORT = parseInt(process.env.PORT, 10) || 3010;
+
+const app = express();
+
+app.listen(PORT, async () => {
+    console.log(`La app esta escuchando en el puerto ${PORT}`)
+    await db.sequelize.sync({force:true});
+});
